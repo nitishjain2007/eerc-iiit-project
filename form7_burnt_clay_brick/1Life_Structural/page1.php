43b2dfcdb56d99fdcf79d3993a1c962b660197a2
nitishjain2007 <nitish.jain@students.iiit.ac.in>
2015-04-11 16:19:13 +0530

    Adding Form 7

---
+++ form7_burnt_clay_brick/1Life_Structural/page1.php
@@ -0,0 +1,91 @@
+
+<!-- Life-threatening Factors related to the STRUCTURAL ELEMENTS - Page 1 -->
+
+<form action="page1-backend.php">
+
+<p>1. Siting Issues</p>
+<p>a. The house is built on hill slopes or elevated mounds that can slide.</p>
+<input type="radio" name="_1a" value="Y" > Yes
+<input type="radio" name="_1a" value="N"> No
+
+<p>b. The house is built on river terraces that can slide/creep.</p>
+<input type="radio" name="_1b" value="Y" > Yes
+<input type="radio" name="_1b" value="N"> No
+
+<p>c. The house is built on hill slopes /adjacent to hill slopes (even though on
+flat ground), but vulnerable to falling debris from the hill top.</p>
+<input type="radio" name="_1c" value="Y" > Yes
+<input type="radio" name="_1c" value="N"> No
+
+
+<p>2. Soil and Foundation Conditions</p>
+<p>a. The soil underneath the building is liquefiable.</p>
+<input type="radio" name="_2a" value="Y"> Yes
+<input type="radio" name="_2a" value="N"> No
+
+
+<p>b. The soil in the area adjoining the site is liquefiable and can flow
+laterally to move the soil from underneath the building.</p>
+<input type="radio" name="_2b" value="Y" > Yes
+<input type="radio" name="_2b" value="N"> No
+
+
+<p>3. Architectural Features</p>
+<p>a. The outer dimensions of the house at plinth level are less than those at
+the top in either of the two horizontal plan directions.</p>
+<input type="radio" name="_3a" value="Y" > Yes
+<input type="radio" name="_3a" value="N"> No
+
+
+<p>b. The house has large una nchored projections and overhangs.</p>
+<input type="radio" name="_3b" value="Y" > Yes
+<input type="radio" name="_3b" value="N"> No
+
+
+<p>c. The door and window openings in walls are at the corners.</p>
+<input type="radio" name="_3c" value="Y" > Yes
+<input type="radio" name="_3c" value="N"> No
+
+
+<p>d. The building is almost touching or located close to an adjacent
+seemingly unsafe building/construction, whose collapse/pounding can
+damage it.</p>
+<input type="radio" name="_3d" value="Y" > Yes
+<input type="radio" name="_3d" value="N"> No
+
+<p>4. Structural Aspects</p>
+<p>a. The roof is constructed such that it is not integral within itself (i.e., it does not act as a single unit and breaks open during earthquake shaking) and is not anchored into the walls.</p>
+<input type="radio" name="_4a" value="Y" > Yes
+<input type="radio" name="_4a" value="N"> No
+
+
+<p>b. The walls are thick and made in two wythes.</p>
+<input type="radio" name="_4b" value="Y" > Yes
+<input type="radio" name="_4b" value="N"> No
+
+
+<p>c. The walls are not integrated into each other at the corners.</p>
+<input type="radio" name="_4c" value="Y" > Yes
+<input type="radio" name="_4c" value="N"> No
+
+
+<p>d. The staircases are not anchored into the walls of the house.</p>
+<input type="radio" name="_4d" value="Y" > Yes
+<input type="radio" name="_4d" value="N"> No
+
+
+<p>5. Construction Details</p>
+<p>a. The walls are made with mud mortar and are exposed to the vagaries of the outside weather (especially rain water beating).</p>
+<input type="radio" name="_5a" value="Y" > Yes
+<input type="radio" name="_5a" value="N"> No
+
+
+<p>b. The walls are made with no mortar.</p>
+<input type="radio" name="_5b" value="Y" > Yes
+<input type="radio" name="_5b" value="N"> No
+
+<br>
+<input type="submit" value="Submit">
+</form>
+
+
---
+++ form7_burnt_clay_brick/4Economic_NonStructural/page1.php
@@ -0,0 +1,44 @@
+<!-- Burnt Clay Brick Masonry House -->
+<!-- Economic Loss-Inducing Factors related to the Non-Structural Elements -->
+
+<form action="page1-backend.php">
+
+<!-- 1 -->
+<p>1. Acceleration and Displacement Hazards</p>
+
+<p>1.1 Shelves</p>
+<p>You may select 0 or more.</p>
+<input type="checkbox" name="_1-1[]" value="a"> Not anchored to structural system
+<input type="checkbox" name="_1-1[]" value="b"> Contents not strapped
+
+<p>1.2 Items on lofts</p>
+<p>You may select 0 or more.</p>
+<input type="checkbox" name="_1-2[]" value="a"> Unsecured
+
+<p>1.3 Hangings from roof/floor and from walls</p>
+<p>You may select 0 or more.</p>
+<input type="checkbox" name="_1-3[]" value="a"> Heavy weight and unstrapped
+
+<p>1.4 Gas cylinders and geysers</p>
+<p>You may select 0 or more.</p>
+<input type="checkbox" name="_1-4[]" value="a"> Unsecured to the wall
+
+<p>1.5 Items on adjacent building</p>
+<p>You may select 0 or more.</p>
+<input type="checkbox" name="_1-5[]" value="a"> Unsecured to the adjacent building
+
+<!-- 2 -->
+<p>2. Lifelines</p>
+
+<p>2.1 Electric supply</p>
+<p>You may select 0 or more.</p>
+<input type="checkbox" name="_2-1[]" value="a"> Cable is taught between house and pole
+
+<p>2.2 Water supply</p>
+<p>You may select 0 or more.</p>
+<input type="checkbox" name="_2-2[]" value="a"> Pipeline is rigidly held between house and mainline
+
+<br>
+<input type="submit" value="submit">
+</form>
+
